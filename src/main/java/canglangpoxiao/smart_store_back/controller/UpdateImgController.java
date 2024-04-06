5203d43b968516164a719cda6b2f6fb4282de719
zhj20 <99gelanjingling@gmail.com>
2024-04-06 11:59:18 +0800

    删掉了阿里云的敏感信息

--- src/main/java/canglangpoxiao/smart_store_back/controller/UpdateImgController.java
+++ src/main/java/canglangpoxiao/smart_store_back/controller/UpdateImgController.java
@@ -49,10 +49,10 @@ public class UpdateImgController {
     public String uploadImg(@Param("uploadfile") MultipartFile uploadfile, @Param("uid") long uid) throws IOException {
 
         // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
+        String endpoint = "";
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI5tNsiEWTM57UKJPfypJQ";
-        String accessKeySecret = "e2qNeyRn5OGFoh4b6ZX2JRAWNPlEql";
+        String accessKeyId = "";
+        String accessKeySecret = "";
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
