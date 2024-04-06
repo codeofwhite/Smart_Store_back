3aa44067025ba1882ef3b00046cbddbd92f08552
zhj20 <99gelanjingling@gmail.com>
2024-04-06 12:00:40 +0800

    删掉了阿里云的敏感信息

--- src/main/java/canglangpoxiao/smart_store_back/controller/UpdateImgController.java
+++ src/main/java/canglangpoxiao/smart_store_back/controller/UpdateImgController.java
@@ -21,11 +21,6 @@ public class UpdateImgController {
 
 
         // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
-        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI5tNsiEWTM57UKJPfypJQ";
-        String accessKeySecret = "e2qNeyRn5OGFoh4b6ZX2JRAWNPlEql";
-        System.out.println(url);
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
