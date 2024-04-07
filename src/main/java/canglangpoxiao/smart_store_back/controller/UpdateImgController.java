75210041081bf100ba027632eaa202daab9dfda3
zhj20 <99gelanjingling@gmail.com>
2024-04-08 00:57:55 +0800

    删除敏感信息

--- src/main/java/canglangpoxiao/smart_store_back/controller/UpdateImgController.java
+++ src/main/java/canglangpoxiao/smart_store_back/controller/UpdateImgController.java
@@ -20,14 +20,7 @@ public class UpdateImgController {
     void testUpdateOss(@Param("url") String url, @Param("uid") long uid) {
 
 
-        // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
-        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI5tNsiEWTM57UKJPfypJQ";
-        String accessKeySecret = "e2qNeyRn5OGFoh4b6ZX2JRAWNPlEql";
-        System.out.println(url);
-        // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
 
         // 上传文件流。
         InputStream inputStream = null;
@@ -36,10 +29,7 @@ public class UpdateImgController {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        // 获取当前时间
-        Date date = new Date();
-        ossClient.putObject("smartstorezzw", date + ".jpg", inputStream);
-        userRepository.updateUserImg("https://smartstorezzw.oss-cn-hangzhou.aliyuncs.com/" + date + ".jpg", uid);
+
 
         // 关闭OSSClient。
         ossClient.shutdown();
@@ -48,13 +38,7 @@ public class UpdateImgController {
     @PostMapping("/uploadImg")
     public String uploadImg(@Param("uploadfile") MultipartFile uploadfile, @Param("uid") long uid) throws IOException {
 
-        // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
-        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-        String accessKeyId = "LTAI5tNsiEWTM57UKJPfypJQ";
-        String accessKeySecret = "e2qNeyRn5OGFoh4b6ZX2JRAWNPlEql";
-        // 创建OSSClient实例。
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
 
         //1、输出测试
         System.out.println("==============");
@@ -64,10 +48,7 @@ public class UpdateImgController {
         //2、将上传的图片存储到云端
         InputStream inputStream = uploadfile.getInputStream();
 
-        // 获取当前时间
-        Date date = new Date();
-        ossClient.putObject("smartstorezzw", date + ".jpg", inputStream);
-        userRepository.updateUserImg("https://smartstorezzw.oss-cn-hangzhou.aliyuncs.com/" + date + ".jpg", uid);
+
 
         // 关闭OSSClient。
         ossClient.shutdown();
