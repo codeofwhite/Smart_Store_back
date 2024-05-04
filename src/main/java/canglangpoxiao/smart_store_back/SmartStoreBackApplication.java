af52dbeb40f88e6064c338515d06cb17667ce69c
zhj20 <99gelanjingling@gmail.com>
2024-05-05 01:02:53 +0800

    加了监视器

--- src/main/java/canglangpoxiao/smart_store_back/SmartStoreBackApplication.java
+++ src/main/java/canglangpoxiao/smart_store_back/SmartStoreBackApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @Configuration
 @MapperScan("canglangpoxiao.smart_store_back.mapper")
 @EnableCaching  //开启缓存
+@EnableAsync // 开启异步线程
 public class SmartStoreBackApplication {
 
     public static void main(String[] args) {
